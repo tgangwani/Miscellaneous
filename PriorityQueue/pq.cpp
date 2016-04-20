33d67ed4d6512df8424168d82ae5b84e5190df52
tgangwani <tanmay2g@gmail.com>
2016-04-19 23:46:26 -0500

    stat collection, code cleanup

--- PriorityQueue/pq.cpp
+++ PriorityQueue/pq.cpp
@@ -1,10 +1,14 @@
-#include<iostream>
-#include<list>
-#include<vector>
-#include"boost/program_options.hpp"
+#include<time.h>
+#include<algorithm>
 #include<pthread.h>
+#include"boost/program_options.hpp"
 #include"pq.h"
 #include"barrier.h"
+
+// when enabled, the program stores the 'data' that was added or removed at
+// each enqueue() and deleteMin() call. This data can be used to implement
+// application specific logic 
+//#define USE_APP  
 
 // define type of priority queue elements
 typedef uint64_t pq_element_t; 
@@ -16,10 +20,6 @@ struct m_pthread_arg{
   uint32_t tid;
 };
 
-// global variables
-uint32_t numThreads;
-pthread_barrier_t barr;
-
 // function to parse the input 
 void parseArgs(int argc, char **argv) {
   
@@ -27,7 +27,9 @@ void parseArgs(int argc, char **argv) {
   po::options_description desc("Options"); 
   desc.add_options() 
     ("help", "Print help messages") 
-    ("t", po::value<uint32_t>(&numThreads)->required(), "Number of threads");
+    ("t", po::value<uint32_t>(&numThreads)->required(), "Number of threads")
+    ("m", po::value<uint64_t>(&numNodes)->default_value(10000), "Number of nodes by each thread")
+    ("hb", "Enable heartbeating");
 
   po::variables_map vm; 
   try 
@@ -39,6 +41,10 @@ void parseArgs(int argc, char **argv) {
       std::cout<<desc<<"\n";
       exit(1);
     }   
+    if(vm.count("hb"))
+    {
+      heartbeat =true;
+    }
   }
   catch (po::error& e)
   {
@@ -47,7 +53,22 @@ void parseArgs(int argc, char **argv) {
     exit(1);
   }
 
-  std::cout<<"Starting program with "<<numThreads<<" thread(s)\n";
+  std::cout<<"Starting program with "<<numThreads<<" thread(s). Each thread inserts " << numNodes << " nodes.\n";
+}
+
+// prints the final stats for the application
+void print_stats() {
+  std::cout<<"+++Stats+++\n";
+  for(uint8_t i = 0; i < NUM_STATS; i++) {
+    std::cout<<stat_map[i]<<"\t"<<final_stats[i]<<"\n";
+  }
+}
+
+// per-thread stat collection
+void collect_stats() {
+  for(uint8_t i = 0; i < NUM_STATS; i++) {
+    final_stats[i].fetch_add((*stats.get())[i]);
+  }
 }
 
 // function called by each pthread
@@ -56,35 +77,53 @@ void* th_fn(void *arg) {
   m_pthread_arg<T> *m_arg = (m_pthread_arg<T>*) arg;
   pq<T> *ptr = m_arg->q_ptr;
   uint32_t thread_id = m_arg->tid;
+  std::vector<T> enq_removals, deq_removals;  // data items removed from queue during enqueue/deleteMin
+  std::vector<T> enq_adds;
+  stats.reset(new std::vector<uint64_t>(NUM_STATS, 0)); // thread-local-storage
 
   // wait for all threads to reach this point
   pthread_barrier_wait(&barr);
-  std::list<T> enq_removals, deq_removals;  // data items removed from queue during enqueue/deleteMin
-  std::list<T> enq_adds;
 
-  for(uint32_t k=0; k<10000; k++) {
-    // dummy : each thread enqueues nodes with some values
-    T data = (T)(thread_id*k+1);
+  #ifdef USE_APP
+    // reserve capacities for fast push_back
+    enq_removals.reserve(numNodes);
+    deq_removals.reserve(numNodes);
+    enq_adds.reserve(numNodes);
+  #endif
+
+  for(uint32_t k=0; k<numNodes; k++) {
+    
+    // generate random priorities in (1, 100) 
+    T data = (T)(rand()%100 + 1);
     ptr->enqueue(data, thread_id, enq_removals, enq_adds);
     ptr->deleteMin(thread_id, deq_removals);
+
+    // periodic heartbeats from thread-0
+    if(heartbeat && thread_id == 0 && k%1000 == 0) {
+      std::cout<<"[Heartbeat] Completed " << k << " nodes.\n";
+    }
   }
 
+  // boost thread-safe logging
   //BOOST_LOG_TRIVIAL(debug) << "[end] " << enq_removals.size() + deq_removals.size() << "\n"; 
   //BOOST_LOG_TRIVIAL(debug) << enq_adds.size();
+  collect_stats();
   return 0;
 }
 
 int main(int argc, char **argv) {
 
   parseArgs(argc, argv);
- 
+
+  srand (time(NULL));
+
   assert(numThreads > 0);
   std::list<pthread_t> pthreads(numThreads-1); 
   std::vector<m_pthread_arg<pq_element_t> > args_list(numThreads-1); 
 
   // init the priority queue
   pq<pq_element_t> Q;
-                     
+  
   // init the pthread barrier
   if(pthread_barrier_init(&barr, NULL, numThreads)){
     std::cout<<"Could not initialize pthread barrier\n";
@@ -112,6 +151,7 @@ int main(int argc, char **argv) {
 
   // prints the priority queue
   Q.print();
+  print_stats();
 
   pthread_barrier_destroy(&barr);
   std::cout<<"Application complete"<<"\n";
--- PriorityQueue/pq.h
+++ PriorityQueue/pq.h
@@ -1,7 +1,12 @@
 // implements the priority queue
+#include<iostream>
+#include<list>
+#include<vector>
 #include<atomic>
 #include<assert.h>
 #include"boost/log/trivial.hpp"
+#include "boost/thread/tss.hpp"
+#include"utils.h"
 
 //#define DEBUG 
 #define ONE static_cast<uintptr_t>(1) 
@@ -10,6 +15,14 @@
 
 // macro to assert if the last bit of a pointer is 0
 #define CHECK_MSB(val) assert((RC(val) & ONE_MSB) == 0);
+
+// global- variables
+uint32_t numThreads = 0; 
+pthread_barrier_t barr;
+uint64_t numNodes = 0;
+boost::thread_specific_ptr<std::vector<uint64_t>> stats;
+std::vector<std::atomic<uint64_t>> final_stats(NUM_STATS);
+bool heartbeat = false;
 
 // node type of the priority queue
 template <typename T>
@@ -47,7 +60,7 @@ class pq {
     }
 
     // function to add to the priority queue
-    void enqueue(T& data, const uint32_t& thread_id, std::list<T>& enq_removals, std::list<T>& enq_adds) {
+    void enqueue(T& data, const uint32_t& thread_id, std::vector<T>& enq_removals, std::vector<T>& enq_adds) {
 
       // create new node. Assert that the msb is zero. We'll use this for
       // logical deletion
@@ -82,9 +95,22 @@ class pq {
           // if the logical bit is set, try to physically remove this node from
           // the priority queue using CAS
           if(atomic_compare_exchange_weak_explicit(prev, &curr, next, std::memory_order_release, std::memory_order_relaxed)) {
-            enq_removals.push_back(curr->data);
+
+            #ifdef STATS
+              (*stats.get())[ENQ_STEAL_SUCC]++; 
+            #endif
+
+            #ifdef USE_APP
+              enq_removals.push_back(curr->data);
+            #endif
+
           }
           else {
+            
+            #ifdef STATS
+              (*stats.get())[ENQ_STEAL_FAIL]++;
+            #endif  
+
             goto start;
           }
 
@@ -110,11 +136,24 @@ class pq {
       
       // found correct position. Now use CAS to insert the node
       // insert node between prev and curr
-      if(!atomic_compare_exchange_weak_explicit(prev, &curr, new_node, std::memory_order_release, std::memory_order_relaxed)) {
-        goto start;
+      if(atomic_compare_exchange_weak_explicit(prev, &curr, new_node, std::memory_order_release, std::memory_order_relaxed)) {
+
+        #ifdef USE_APP
+          enq_adds.push_back(new_node->data);
+        #endif
+        
+        #ifdef STATS
+          (*stats.get())[ENQ_SUCC]++;
+        #endif  
       }
       else {
-        enq_adds.push_back(new_node->data);
+
+        #ifdef STATS
+          (*stats.get())[ENQ_FAIL]++;
+        #endif  
+        
+        // enqueue failed. Try again!
+        goto start;
       }
 
       #ifdef DEBUG
@@ -123,7 +162,7 @@ class pq {
     }
 
     // function to remove the top of the priority queue
-    void deleteMin(const uint32_t& thread_id, std::list<T>& deq_removals) {
+    void deleteMin(const uint32_t& thread_id, std::vector<T>& deq_removals) {
       
       while(true) {
         node<T> *first = head.load(std::memory_order_relaxed);    // load the head
@@ -142,7 +181,7 @@ class pq {
     }
 
     // function return true if the thread could logically remove a node 
-    bool tryRemove(node<T> *first, std::list<T>& deq_removals, const uint32_t& thread_id) {
+    bool tryRemove(node<T> *first, std::vector<T>& deq_removals, const uint32_t& thread_id) {
 
       // get the <next, d> pair from first->next
       node<T> *next = first->next;
@@ -158,8 +197,19 @@ class pq {
 
         // try to physically remove this node using CAS
         if(atomic_compare_exchange_weak_explicit(&head, &first, next, std::memory_order_release, std::memory_order_relaxed)) {
-          deq_removals.push_back(first->data);
+          #ifdef USE_APP
+            deq_removals.push_back(first->data);
+          #endif
+
+          #ifdef STATS
+            (*stats.get())[DEQ_STEAL_SUCC]++;
+          #endif  
         }
+        #ifdef STATS
+        else {
+          (*stats.get())[DEQ_STEAL_FAIL]++;  
+        }
+        #endif
          
         return false;
       }
@@ -169,14 +219,35 @@ class pq {
       auto n = reinterpret_cast<std::atomic<node<T>*>*> (&first->next);
 
       if(!atomic_compare_exchange_weak_explicit(n, &next, updated_next, std::memory_order_release, std::memory_order_relaxed)) {
+
+        #ifdef STATS
+          (*stats.get())[DEQ_LOG_FAIL]++;
+        #endif
+
         // logical deletion failure
         return false;
       }
+      #ifdef STATS
+      else {
+        (*stats.get())[DEQ_LOG_SUCC]++;
+      }
+      #endif
       
       // try physical removal
       if(atomic_compare_exchange_weak_explicit(&head, &first, next, std::memory_order_release, std::memory_order_relaxed)) {
-        deq_removals.push_back(first->data);
+        #ifdef USE_APP
+          deq_removals.push_back(first->data);
+        #endif
+        
+        #ifdef STATS
+          (*stats.get())[DEQ_PHY_SUCC]++;
+        #endif
       }
+      #ifdef STATS
+      else {
+        (*stats.get())[DEQ_PHY_FAIL]++;
+      }
+      #endif
  
       // logical deletion success
       return true;
